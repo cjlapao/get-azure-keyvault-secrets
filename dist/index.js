4aa7cc8b43d30318983b2f2fbf9192ef7ffe9cd3
Carlos Lapao <cjlapao@gmail.com>
2024-09-11 08:27:53 +0100

    fixed transpiled code

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -65225,3 +65225,4 @@ if (require.main === require.cache[eval('__filename')]) {
 module.exports = __webpack_exports__;
 /******/ })()
 ;
+//# sourceMappingURL=index.js.map
